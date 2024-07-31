ce999636598422d89e9c6ac38b12a8dadbd111af
insomniac206 <harishabhinav027@gmail.com>
2024-07-31 10:12:16 +0530

    set up application factory (flask docs, tutorial)

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+def create_app():
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile("config.py")
+
+    with app.app_context():
+
+        from . import routes
+
+        return app
